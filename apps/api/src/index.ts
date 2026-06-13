13a204bef7c86504fa087e8e3b3da8e461304267
Abu Bakkar Siddique <abubakkar2502@gmail.com>
2026-06-13 23:20:45 +0530

    feat: api index file to run the api and env example

---
+++ apps/api/src/index.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { buildApp } from "./app";
+
+const PORT = parseInt(process.env.PORT ?? "3001", 10);
+const HOST = process.env.HOST ?? "0.0.0.0";
+
+async function main() {
+  const app = await buildApp();
+
+  try {
+    await app.listen({ port: PORT, host: HOST });
+    app.log.info(`API running at http://${HOST}:${PORT}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+}
+
+main();
